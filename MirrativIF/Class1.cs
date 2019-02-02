91b945880d0735da5e056f089eb9e86c69c6a997
Ryu Shinmura <rysestock@gmail.com>
2019-02-03 01:20:54 +0900

    Add project files.

---
+++ MirrativIF/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirrativIF
+{
+    public class Class1
+    {
+    }
+}
