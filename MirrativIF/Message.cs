636cf2ab2d5f6bd4952e814009259f356d6e4547
Ryu Shinmura <rysestock@gmail.com>
2019-02-03 01:42:38 +0900

    メッセージ関連のインタフェースを作成

---
+++ MirrativIF/Message.cs
@@ -0,0 +1,42 @@
+﻿using SitePlugin;
+
+namespace MirrativSitePlugin
+{
+    public enum MirrativMessageType
+    {
+        Unknown,
+        Comment,
+        JoinRoom,
+        Connected,
+        Disconnected,
+    }
+
+    public interface IMirrativMessage : IMessage
+    {
+        MirrativMessageType MirrativMessageType { get; }
+    }
+    public interface IMirrativConnected : IMirrativMessage
+    {
+    }
+    public interface IMirrativDisconnected : IMirrativMessage
+    {
+    }
+    public interface IMirrativComment : IMirrativMessage
+    {
+        //string Comment { get; }
+        string Id { get; }
+        //string UserName { get; }
+        string UserId { get; }
+        long PostedAt { get; }
+        IMessageImage UserIcon { get; set; }
+    }
+    public interface IMirrativJoinRoom : IMirrativMessage
+    {
+        //string Comment { get; }
+        string Id { get; }
+        //string UserName { get; }
+        string UserId { get; }
+        long PostedAt { get; }
+        IMessageImage UserIcon { get; set; }
+    }
+}
